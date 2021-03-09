e3556530a58149fd4fc417da1f4850bb6a479002
Ben Gardiner <bennygards@gmail.com>
2021-03-10 10:32:56 +1100

    player data entered from 11-13/14

--- nbaDatabase/Player.sql
+++ nbaDatabase/Player.sql
@@ -1,12 +1,14 @@
 ﻿CREATE TABLE [dbo].[Player]
 (
-   PLAYER_ID		 INTEGER  NOT NULL PRIMARY KEY
-  ,PLAYER_NAME		 VARCHAR(19) NOT NULL
-  ,AGE               INTEGER NOT NULL 
-  ,GP                INTEGER NOT NULL 
-  ,W                 INTEGER NOT NULL 
-  ,L                 INTEGER NOT NULL 
-  ,W_PCT             NUMERIC(5,3) NOT NULL
+   TEAM_ABBREVIATION  VARCHAR(3) NOT NULL 
+  ,SEASON_YEAR		  INTEGER NOT NULL
+  ,PLAYER_ID		  INTEGER  NOT NULL 
+  ,PLAYER_NAME		  VARCHAR(30) NOT NULL
+  ,AGE                INTEGER NOT NULL 
+  ,GP                 INTEGER NOT NULL 
+  ,W                  INTEGER NOT NULL 
+  ,L                  INTEGER NOT NULL 
+  ,W_PCT              NUMERIC(5,3) NOT NULL
   ,MINS               NUMERIC(4,1) NOT NULL
   ,FGM               NUMERIC(3,1) NOT NULL
   ,FGA               NUMERIC(4,1) NOT NULL
@@ -30,4 +32,5 @@
   ,PTS               NUMERIC(4,1) NOT NULL
   ,PLUS_MINUS        NUMERIC(4,1) NOT NULL
   ,NBA_FANTASY_PTS   NUMERIC(4,1) NOT NULL
+  PRIMARY KEY (TEAM_ABBREVIATION, SEASON_YEAR, PLAYER_ID)
 )
--- nbaDatabase/Season.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Season]
-(
-	SEASON_YEAR INTEGER,
-	PLAYER_ID INTEGER,
-	TEAM_ABBREVIATION VARCHAR(3),
-	primary key (SEASON_YEAR, PLAYER_ID, TEAM_ABBREVIATION), 
-	constraint fk_season_playerid foreign key (PLAYER_ID) references Player (player_id),
-	constraint fk_season_teamName foreign key (TEAM_ABBREVIATION) references Team (TEAM_ABBREVIATION)
-)
