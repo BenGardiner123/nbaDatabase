59eb6aba8c4dc5c95d24ba331e20b1c6de3daba0
Ben Gardiner <bennygards@gmail.com>
2021-03-11 13:18:25 +1100

    developing algo fagainst testing data - ready for the future data

---
+++ AlternativeAlgo.sql
@@ -0,0 +1,8 @@
+﻿select PlayerAdv.Season, playeradv.tm, COUNT(*) as totalPlayers 
+from PlayerAdv
+where (G Between 50 AND 82)
+AND (MP Between 900 AND 3167)
+AND (PER Between 12.9 AND 129)
+AND (WS between 1.3 and 19)
+group by playeradv.tm, PlayerAdv.Season
+order by PlayerAdv.Season ASC, totalPlayers desc
